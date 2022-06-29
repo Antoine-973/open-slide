f2119377d96c0864c494698d26cd970d4f78a0a1
Antoine Saunier <antoine.saunier.pro@gmail.com>
2022-06-29 16:02:19 +0200

    add basic ui for the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import "./App.css";
 import {Slide} from "./Components/Pages/Slide/Slide";
+import {DocumentList} from "./Components/Pages/DocumentList/DocumentList";
 import {Login} from "./Components/Pages/login/Login" ;
 
 export const App = () => {
-  return (
-      <div className="App">
-        <h1>Welcome to React Router!</h1>
-        <Routes>
-            {/*<Route path="/" element={<Home />} />*/}
-            {/*<Route path="about" element={<Login />} />*/}
-            {/*<Route path="about" element={<Document />} />*/}
-            <Route path="slide" element={<Slide />} />
-            <Route path="login" element={<Login />} />
-        </Routes>
-      </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<DocumentList />} />
+                {/*<Route path="about" element={<Login />} />*/}
+                {/*<Route path="about" element={<Document />} />*/}
+                <Route path="slide" element={<Slide/>}/>
+                <Route path="login" element={<Login/>}/>
+            </Routes>
+        </div>
+    );
 }
