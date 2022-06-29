9c8d733fe89e538e74a98246ccf2e9f4d60dd4de
Alexandrebdry <alexbaudry0@gmail.com>
2022-06-29 14:54:29 +0200

    Feature Firebase login
    Adding login front

--- src/Components/Pages/login/Login.jsx
+++ src/Components/Pages/login/Login.jsx
@@ -1,6 +1,10 @@
 import $ from 'react'
 import login from '../../../firebase/firebase' ;
-
+import GoogleIcon from '@mui/icons-material/Google';
+import {Button, Card, CardContent, IconButton, Typography} from "@mui/material";
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 let user = {} ;
 
@@ -12,11 +16,25 @@ const setLogin = (loggedUser) => {
 export const Login = () => {
     return(
         <>
-            <div>
-                <span>SIgn-in know with Google</span>
-                <button onClick={(evt)=> login(setLogin)}>GOOGLE SIGN IN</button>
-            </div>
+            <CssBaseline>
+                <Container maxWidth="sm">
+                    <div>
+                        <Button aria-label="Google sign-in" size="large" onClick={(evt)=> login(setLogin)}>
+                            <Card sx={{ minWidth: 275 }}>
+                                <CardContent>
+                                    <Typography variant="h5" component="div">
+                                        <GoogleIcon sx={{fontSize: 56}}/>
+                                    </Typography>
+                                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                        Sign-in with Google
+                                    </Typography>
 
+                                </CardContent>
+                            </Card>
+                        </Button>
+                    </div>
+                </Container>
+            </CssBaseline>
         </>
     )
 }
