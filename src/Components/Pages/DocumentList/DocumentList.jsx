76b11b2a4b7b0f9d826584fd3aa18b4c0ddea20e
Arthur <arthur@LAPTOP-VNKK2NM9.localdomain>
2022-07-26 23:21:31 +0200

    add list des documents sur la home ete ajout de la suppression

--- src/Components/Pages/DocumentList/DocumentList.jsx
+++ src/Components/Pages/DocumentList/DocumentList.jsx
@@ -1,46 +1,75 @@
-import  {useContext} from 'react'
-import {Button, Grid, Typography} from "@mui/material";
+import {useContext, useEffect, useState} from 'react'
+import {Button, Container, Grid, IconButton, Paper, Typography} from "@mui/material";
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import Navbar from "../../Libs/Navbar";
-import {AuthContext} from "../../Provider/AuthProvider";
-
-const documents = [
-    {
-        "title": "purple",
-        "content": "minivan",
-        "registration": new Date('2017-01-03'),
-        "capacity": 7
-    },
-    {
-        "color": "red",
-        "type": "station wagon",
-        "registration": new Date('2018-03-03'),
-        "capacity": 5
-    },
-]
+import {child, get, ref, remove} from "firebase/database";
+import {db} from "../../../firebase/firebase";
+import CloseIcon from '@mui/icons-material/Close';
 
 export const DocumentList = () => {
 
-    const {  auth, setAuthData } = useContext(AuthContext);
-    console.log(auth) ;
+    const [loading, setLoading] = useState(true);
+    const [showDelete, setShowDelete] = useState(false);
+    const [documents, setDocuments] = useState([]);
+    const [deletion, setDeletion] = useState(false);
 
-    return(
+    const deleteDocument = (document) => {
+        remove(ref(db,'/documents/'+document.id));
+        if (deletion) {
+            setDeletion(false);
+        }else {
+            setDeletion(true);
+        }
+    }
+
+    useEffect(() => {
+        get(child(ref(db),'/documents')).then(snapshot => {
+            if (snapshot.exists()) {
+                const docs = [];
+                Object.entries(snapshot.val()).forEach(([key, value]) => {
+                    docs.push({id: key, ...value})
+                })
+                setDocuments(docs);
+            } else {
+                console.log('no data');
+            }
+        }).finally(() => {
+            setLoading(false);
+        });
+    }, [deletion]);
+
+    return (
+        !loading &&
         <Grid container direction={"column"} gap={6}>
             <Grid item>
                 <Navbar/>
             </Grid>
             <Grid item>
-                <Button size={"large"} variant={"contained"} startIcon={<AddBoxIcon />}>
+                <Button size={"large"} variant={"contained"} startIcon={<AddBoxIcon/>}>
                     Créer une présentation
                 </Button>
             </Grid>
-            <Grid item container border={1} borderColor={"red"} justifyItems={"flex-start"} gap={2} px={6}>
-                <Typography  variant={"h6"}>Mes présentations</Typography>
-                <Grid container direction={"row"} justifyContent={"flex-start"} alignItems={"flex-start"} wrap gap={6}>
-                    <Grid item bgcolor={"red"} width={208} height={163}>
-                        Test
-                    </Grid>
-                </Grid>
+            <Typography variant={"h6"}>Mes présentations</Typography>
+
+            <Grid container justifyContent={"center"} gap={6}>
+                {
+                    documents.map((document,key) => {
+                        return (
+                            <Paper key={key} elevation={8} variant={"elevation"} >
+                                <Grid width={200} height={100} padding={4} textAlign={"center"} position={"relative"} display={"block"} >
+                                        <Grid item position={"absolute"} top={0} right={0} margin={0}>
+                                            <IconButton onClick={() => {
+                                                deleteDocument(document);
+                                            }}>
+                                                <CloseIcon/>
+                                            </IconButton>
+                                        </Grid>
+                                    <Typography>{document.title}</Typography>
+                                </Grid>
+                            </Paper>
+                        )
+                    })
+                }
             </Grid>
         </Grid>
     )
