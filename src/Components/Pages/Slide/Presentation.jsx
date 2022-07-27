f8919b3ee06d1b3cf9c20655feb72cad97a78032
Arthur <arthur@LAPTOP-VNKK2NM9.localdomain>
2022-07-27 06:03:19 +0200

    fix/affichage diapo

--- src/Components/Pages/Slide/Presentation.jsx
+++ src/Components/Pages/Slide/Presentation.jsx
@@ -3,18 +3,18 @@ import Reveal from 'reveal.js';
 import 'reveal.js/dist/reset.css';
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/white.css';
-import { makeStyles } from '@mui/styles';
+import {makeStyles} from '@mui/styles';
 import {Grid, IconButton} from "@mui/material";
 import './presentation.css'
 import CloseIcon from "@mui/icons-material/Close";
 import {useNavigate, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
-import {onValue, ref} from "@firebase/database";
+import {onValue, ref} from "firebase/database";
 import {db} from "../../../firebase/firebase";
 
 const useStyles = makeStyles({
     slideContainer: {
-        zIndex:100,
+        zIndex: 100,
     },
 });
 
@@ -22,71 +22,63 @@ export function Presentation() {
     const classes = useStyles();
     const {id} = useParams();
     const navigate = useNavigate();
-    const [document, setDocument] = useState({loading:true, data: {}});
+    const [document, setDocument] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-
-    const initDocument = () => {
-        const path = ref(db, 'documents/' + id);
-        onValue(path, (data) => {
-                setDocument({loading:false, data: {key : data.key, ...data.val()}});
-            }
-            , (error) => {
+    const initDocument = async () => {
+        const path = ref(db, 'documents/' + id + '/slides');
+        await onValue(path, (data) => {
+                setDocument(data.val());
+            }, (error) => {
                 console.log(error);
             }
         );
+        setLoading(false);
     }
-
     useEffect(() => {
         initDocument();
     }, []);
 
+    useEffect(() => {
+        if (document.length > 0) {
+            let deck = new Reveal({
+                backgroundTransition: 'slide',
+                transition: 'slide',
+                viewDistance: 100,
+                controlsLayout: 'edges',
+                progress: true,
+                slideNumber: 'c/t'
+            })
+            deck.initialize();
+        }
+    }, [document]);
+
     const closePres = () => {
-        navigate('/slide/'+id);
+        navigate('/slide/' + id);
     }
 
-    React.useEffect(() => {
-        let deck = new Reveal({
-            backgroundTransition: 'slide',
-            transition: 'slide',
-            viewDistance:2,
-            controlsLayout: 'edges',
-            progress: true,
-            slideNumber: 'c/t'
-        })
-        deck.initialize();
-    },[])
-    console.log(document)
     return (
-        <Grid className={classes.slideContainer}>
-            <Grid position={"absolute"} zIndex={10} top={0} left={0} padding={1}>
-                <IconButton onClick={() => { closePres() }}>
-                    <CloseIcon/>
-                </IconButton>
-            </Grid>
-            <div className="reveal">
-                <div className="slides" data-transition="slide">
-                    {
-                        document.data?.slides?.map((slide, index) => {
-                            console.log(slide)
-                            return (
-                                <section key={index} data-transition="slide">
-                                    git a
-                                </section>
-                            )
-                        })
-                    }
-                    {/*<section data-transition="slide" data-background-color="aquamarine">*/}
-                    {/*    <h1>🍦</h1>*/}
-                    {/*</section>*/}
-                    {/*<section data-transition="slide" data-background-color="#283b95">*/}
-                    {/*    <h1>🍰🍰🍰</h1>*/}
-                    {/*</section>*/}
-                    {/*<section data-transition="slide">*/}
-                    {/*    miam*/}
-                    {/*</section>*/}
+            <Grid className={classes.slideContainer}>
+                <Grid position={"absolute"} zIndex={10} top={0} left={0} padding={1}>
+                    <IconButton onClick={() => {
+                        closePres()
+                    }}>
+                        <CloseIcon/>
+                    </IconButton>
+                </Grid>
+                <div className="reveal">
+                    <div className="slides" data-transition="slide">
+                        {
+                            document && document.map((slide, index) => {
+                                return (
+                                    <section key={index} data-transition="slide">
+                                        {slide.content}
+                                    </section>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
-            </div>
-        </Grid>
-
+            </Grid>
     );
 }
